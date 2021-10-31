3577d0fd4add34622f8259e0c880c221925ece5a
ArniStarkware <arnon@starkware.co>
2021-10-31 15:16:54 +0200

    taking the STDOUT

--- q1/e3.py
+++ q1/e3.py
@@ -1,2 +1,11 @@
+from io import StringIO  # Python 3
+import sys
+
 class StandardOutput:
-    pass # TODO
+    def __enter__(self):
+        self.string_writer = StringIO()
+        sys.stdout = self.string_writer
+        return self
+    def __exit__(self, exception, error, traceback):
+        self.value = self.string_writer.getvalue()
+        sys.stdout = sys.__stdout__
