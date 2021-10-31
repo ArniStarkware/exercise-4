314e1413e6c226afa736b50b85a236e80af041ef
ArniStarkware <arnon@starkware.co>
2021-10-31 11:47:06 +0200

    Solved Q1

--- q1/e1.py
+++ q1/e1.py
@@ -1,2 +1,9 @@
+import time
 class Timer:
-    pass # TODO
+    def __enter__(self):
+        self.started = time.time()
+        return self
+    def __exit__(self, exception, error, traceback):
+        self.stopped = time.time()
+        self.elapsed = self.stopped - self.started
+
