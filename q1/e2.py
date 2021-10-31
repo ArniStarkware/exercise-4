5c81906aee91235f6d21d0d2faaa2eb6a5d76652
ArniStarkware <arnon@starkware.co>
2021-10-31 13:29:47 +0200

    Suppress error

--- q1/e2.py
+++ q1/e2.py
@@ -1,2 +1,11 @@
+from attr import set_run_validators
+
+
 class Suppress:
-    pass # TODO
+    def __enter__(self):
+        pass
+    def __init__(self, *args):
+        self.exceptions = args
+    def __exit__(self, exception, error, traceback):
+        if len(self.exceptions) == 0 or exception in self.exceptions:
+            return True
