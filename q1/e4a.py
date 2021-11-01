ba974a1ed8b401b749575ed25c2ea15a4fc5aeec
ArniStarkware <arnon@starkware.co>
2021-11-01 15:46:45 +0200

    temp file and temp dir

--- q1/e4a.py
+++ q1/e4a.py
@@ -1,2 +1,8 @@
+import tempfile
+import os
 class TemporaryFile:
-    pass # TODO
+    def __enter__(self):
+        self.fd, self.path = tempfile.mkstemp()
+        return self.path
+    def __exit__(self, exception, error, traceback):
+        os.remove(self.path)
--- q1/e4b.py
+++ q1/e4b.py
@@ -1,2 +1,8 @@
+import tempfile
+import os
 class TemporaryDirectory:
-    pass # TODO
+    def __enter__(self):
+        self.path = tempfile.mkdtemp()
+        return self.path
+    def __exit__(self, exception, error, traceback):
+        os.rmdir(self.path)
