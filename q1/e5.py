18f25048e420659f197a906e8cd4902f9749f025
ArniStarkware <arnon@starkware.co>
2021-11-02 11:20:21 +0200

    Context managers using generators and decorators

--- q1/e5.py
+++ q1/e5.py
@@ -1,18 +1,58 @@
+import contextlib
+import time
+from io import StringIO 
+import sys
+import tempfile
+import os
+
+@contextlib.contextmanager
 def timer():
-    pass # TODO
+    started = time.time()
+    class timerObject:
+        def __init__(self) -> None:
+            self.started = started
+    retObj = timerObject()
+    try:
+        yield retObj
+    finally:
+        stopped = time.time()
+        elapsed = stopped - started
+        retObj.stopped = stopped
+        retObj.elapsed = elapsed
 
-
+@contextlib.contextmanager
 def suppress(*exceptions):
-    pass # TODO
+    if exceptions:
+        try:
+            yield
+        except exceptions:
+            pass
+    else:
+        try:
+            yield
+        except:
+            pass
 
 
+@contextlib.contextmanager
 def standard_output():
-    pass # TODO
+    class stdoutObject:
+        def __init__(self) -> None:
+            self.string_writer = StringIO()
+    retObj = stdoutObject()
+    sys.stdout = retObj.string_writer
+    yield retObj
+    retObj.value = retObj.string_writer.getvalue()
+    sys.stdout = sys.__stdout__
 
-
+@contextlib.contextmanager
 def temporary_file():
-    pass # TODO
+    _ , path = tempfile.mkstemp()
+    yield path
+    os.remove(path)
 
-
+@contextlib.contextmanager
 def temporary_directory():
-    pass # TODO
+    path = tempfile.mkdtemp()
+    yield path
+    os.rmdir(path)
