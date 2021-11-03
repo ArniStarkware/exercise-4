1a349c9f3858170dc1286193ed5e1bdc635e06a3
ArniStarkware <arnon@starkware.co>
2021-11-03 13:33:43 +0200

    ContextManager

--- q1/e6.py
+++ q1/e6.py
@@ -1,2 +1,17 @@
+from contextlib import suppress
+from os import path
+
+
 class ContextManager:
-    pass # TODO
+    def __init__(self,generator) -> None:
+        self.generator = generator
+    
+    def __enter__(self):
+        self.execution = self.generator()
+        return next(self.execution)
+    def __exit__(self, exception, error, traceback):
+        with suppress(StopIteration):
+            if exception:
+                self.execution.throw(exception)
+            else:
+                next(self.execution)
